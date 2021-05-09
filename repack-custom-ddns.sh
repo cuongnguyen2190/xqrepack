426d13989840f8cb566cc83f15069a0835053a92
Cuong Nguyen <smalldragonvt@gmail.com>
2021-05-09 07:24:46 +0000

    add custom DDNS

--- repack-custom-ddns.sh
+++ repack-custom-ddns.sh
@@ -16,20 +16,24 @@ ROOTPW='$1$qtLLI4cm$c0v3yxzYPI46s28rbAYG//'  # "password"
 [ -e "$IMG" ] || { echo "rootfs img not found $IMG"; exit 1; }
 
 # verify programs exist
-command -v unsquashfs &>/dev/null || { echo "install unsquashfs"; exit 1; }
-mksquashfs -version >/dev/null || { echo "install mksquashfs"; exit 1; }
+command -v unsquashfs || { echo "install unsquashfs"; exit 1; }
+mksquashfs -version || { echo "install mksquashfs"; exit 1; }
 
 FSDIR=`mktemp -d /tmp/resquash-rootfs.XXXXX`
 trap "rm -rf $FSDIR" EXIT
 
 # test mknod privileges
-mknod "$FSDIR/foo" c 0 0 2>/dev/null || { echo "need to be run with fakeroot"; exit 1; }
+mknod "$FSDIR/foo" c 0 0 || { echo "need to be run with fakeroot"; exit 1; }
 rm -f "$FSDIR/foo"
 
 >&2 echo "unpacking squashfs..."
 unsquashfs -f -d "$FSDIR" "$IMG"
 
 >&2 echo "patching squashfs..."
+
+# create /opt dir
+mkdir "$FSDIR/opt"
+chmod 755 "$FSDIR/opt"
 
 # modify dropbear init
 sed -i 's/channel=.*/channel=release2/' "$FSDIR/etc/init.d/dropbear"
@@ -60,7 +64,7 @@ enable_dev_access() {
 	nvram commit
 }
 
-boot_hook_add preinit_main enable_dev_access
+boot_hook_add preinit_main enable__access
 NVRAM
 
 # modify root password
